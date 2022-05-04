ef1b1309530d9c1c5711e1a05d341c21d070b81d
Manimaran K V <54267074+kingmanimarankv@users.noreply.github.com>
2022-05-04 15:50:26 +0530

    Add files via upload

--- weather.dart
+++ weather.dart
@@ -1,293 +1,214 @@
 import 'dart:convert';
 
-import 'package:farmtrack/models/weather.dart';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+Weather weatherFromJson(String str) => Weather.fromJson(json.decode(str));
 
-class WeatherScreen extends StatefulWidget {
-  const WeatherScreen({Key? key}) : super(key: key);
+String weatherToJson(Weather data) => json.encode(data.toJson());
 
-  @override
-  State<WeatherScreen> createState() => _WeatherScreenState();
+class Weather {
+  Weather({
+    required this.coord,
+    required this.weather,
+    required this.base,
+    required this.main,
+    required this.visibility,
+    required this.wind,
+    required this.clouds,
+    required this.dt,
+    required this.sys,
+    required this.timezone,
+    required this.id,
+    required this.name,
+    required this.cod,
+  });
+
+  Coord coord;
+  List<WeatherElement> weather;
+  String base;
+  Main main;
+  int visibility;
+  Wind wind;
+  Clouds clouds;
+  int dt;
+  Sys sys;
+  int timezone;
+  int id;
+  String name;
+  int cod;
+
+  factory Weather.fromJson(Map<String, dynamic> json) => Weather(
+        coord: Coord.fromJson(json["coord"]),
+        weather: List<WeatherElement>.from(
+            json["weather"].map((x) => WeatherElement.fromJson(x))),
+        base: json["base"],
+        main: Main.fromJson(json["main"]),
+        visibility: json["visibility"],
+        wind: Wind.fromJson(json["wind"]),
+        clouds: Clouds.fromJson(json["clouds"]),
+        dt: json["dt"],
+        sys: Sys.fromJson(json["sys"]),
+        timezone: json["timezone"],
+        id: json["id"],
+        name: json["name"],
+        cod: json["cod"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "coord": coord.toJson(),
+        "weather": List<dynamic>.from(weather.map((x) => x.toJson())),
+        "base": base,
+        "main": main.toJson(),
+        "visibility": visibility,
+        "wind": wind.toJson(),
+        "clouds": clouds.toJson(),
+        "dt": dt,
+        "sys": sys.toJson(),
+        "timezone": timezone,
+        "id": id,
+        "name": name,
+        "cod": cod,
+      };
 }
 
-class _WeatherScreenState extends State<WeatherScreen> {
-  final _height = 0.0;
-  final _width = 0.0;
-  bool _loading = false;
+class Clouds {
+  Clouds({
+    required this.all,
+  });
 
-  bool _isWeatherFetched = false;
+  int all;
 
-  final TextEditingController _latitudeController = TextEditingController();
-  final TextEditingController _longitudeController = TextEditingController();
-  late String latitude;
-  late String longitude;
+  factory Clouds.fromJson(Map<String, dynamic> json) => Clouds(
+        all: json["all"],
+      );
 
-  late Weather weather;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "Weather Reports",
-            style: TextStyle(
-              fontFamily: "Poppins",
-              fontSize: 20,
-              fontWeight: FontWeight.w500,
-              color: Colors.white,
-            ),
-          ),
-          backgroundColor: Colors.green,
-        ),
-        body: SafeArea(
-            child: SingleChildScrollView(
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-              Container(height: 2, color: Colors.green),
-              _loading
-                  ? const LinearProgressIndicator(
-                      backgroundColor: Colors.white,
-                      color: Colors.green,
-                    )
-                  : Container(),
-              Container(
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text("Weather Reports.",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                                fontFamily: "Poppins",
-                                fontWeight: FontWeight.w700,
-                                fontSize: 20,
-                                color: Colors.black54)),
-                        Form(
-                          child: Column(children: [
-                            latitudeTextFormField(),
-                            longitudeTextFormField()
-                          ]),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            GestureDetector(
-                              onTap: () async {
-                                if (_latitudeController.text.isNotEmpty &&
-                                    _longitudeController.text.isNotEmpty) {
-                                  await getWeather(latitude, longitude);
-                                  setState(() {
-                                    _isWeatherFetched = true;
-                                  });
-                                }
-                              },
-                              child: Container(
-                                height: 50,
-                                width: 100,
-                                decoration: BoxDecoration(
-                                    color: Colors.green,
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: const Center(
-                                    child: Text(
-                                  "Submit",
-                                  style: TextStyle(
-                                    fontFamily: "Poppins",
-                                    fontSize: 13,
-                                    fontWeight: FontWeight.w500,
-                                    color: Colors.white,
-                                  ),
-                                )),
-                              ),
-                            ),
-                            const SizedBox(width: 20),
-                            GestureDetector(
-                              onTap: () {
-                                setState(() {
-                                  _latitudeController.clear();
-                                  _longitudeController.clear();
-                                  _isWeatherFetched = false;
-                                });
-                              },
-                              child: Container(
-                                height: 50,
-                                width: 100,
-                                decoration: BoxDecoration(
-                                    color: Colors.redAccent,
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: const Center(
-                                    child: Text(
-                                  "Reset",
-                                  style: TextStyle(
-                                    fontFamily: "Poppins",
-                                    fontSize: 13,
-                                    fontWeight: FontWeight.w500,
-                                    color: Colors.white,
-                                  ),
-                                )),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ])),
-              _isWeatherFetched
-                  ? Container(
-                      width: double.infinity,
-                      padding: const EdgeInsets.all(20),
-                      color: Colors.grey.shade100,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Temperature : ${weather.main.temp}",
-                              style: const TextStyle(
-                                fontFamily: "Poppins",
-                                fontSize: 13,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.black,
-                              )),
-                          Text("Temperature (Max) : ${weather.main.tempMax}",
-                              style: const TextStyle(
-                                fontFamily: "Poppins",
-                                fontSize: 11,
-                                fontWeight: FontWeight.w300,
-                                color: Colors.black,
-                              )),
-                          Text("Temperature (Min) : ${weather.main.tempMax}",
-                              style: const TextStyle(
-                                fontFamily: "Poppins",
-                                fontSize: 11,
-                                fontWeight: FontWeight.w300,
-                                color: Colors.black,
-                              )),
-                          const Divider(),
-                          Text("Pressure : ${weather.main.pressure}",
-                              style: const TextStyle(
-                                fontFamily: "Poppins",
-                                fontSize: 13,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.black,
-                              )),
-                          const Divider(),
-                          Text("Humidity : ${weather.main.humidity}",
-                              style: const TextStyle(
-                                fontFamily: "Poppins",
-                                fontSize: 13,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.black,
-                              )),
-                          const Divider(),
-                          Text("Wind Speed : ${weather.wind.speed}",
-                              style: const TextStyle(
-                                fontFamily: "Poppins",
-                                fontSize: 13,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.black,
-                              )),
-                          Text("Degree : ${weather.wind.deg}",
-                              style: const TextStyle(
-                                fontFamily: "Poppins",
-                                fontSize: 11,
-                                fontWeight: FontWeight.w300,
-                                color: Colors.black,
-                              )),
-                        ],
-                      ))
-                  : Container()
-            ]))));
-  }
+  Map<String, dynamic> toJson() => {
+        "all": all,
+      };
+}
 
-  Widget latitudeTextFormField() {
-    return Container(
-      margin: const EdgeInsets.only(top: 10),
-      padding: const EdgeInsets.only(left: 10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(
-          color: Colors.black54,
-        ),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: TextFormField(
-        controller: _latitudeController,
-        onFieldSubmitted: (text) {
-          setState(() {
-            latitude = text;
-          });
-        },
-        validator: (text) {
-          if (text == null || text.isEmpty) {
-            return 'Please enter a valid value for latitude';
-          }
-          return null;
-        },
-        keyboardType: TextInputType.number,
-        textInputAction: TextInputAction.done,
-        style: const TextStyle(
-          fontFamily: "Poppins",
-          fontSize: 13,
-          fontWeight: FontWeight.w500,
-          color: Colors.black54,
-        ),
-        decoration: const InputDecoration(
-            border: InputBorder.none, hintText: "Latitude"),
-      ),
-    );
-  }
+class Coord {
+  Coord({
+    required this.lon,
+    required this.lat,
+  });
 
-  Widget longitudeTextFormField() {
-    return Container(
-      margin: const EdgeInsets.only(top: 10),
-      padding: const EdgeInsets.only(left: 10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(
-          color: Colors.black54,
-        ),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: TextFormField(
-        controller: _longitudeController,
-        onFieldSubmitted: (text) {
-          setState(() {
-            longitude = text;
-          });
-        },
-        validator: (text) {
-          if (text == null || text.isEmpty) {
-            return 'Please enter a valid value for longitude';
-          }
-          return null;
-        },
-        keyboardType: TextInputType.number,
-        textInputAction: TextInputAction.done,
-        style: const TextStyle(
-          fontFamily: "Poppins",
-          fontSize: 13,
-          fontWeight: FontWeight.w500,
-          color: Colors.black54,
-        ),
-        decoration: const InputDecoration(
-            border: InputBorder.none, hintText: "Longitude"),
-      ),
-    );
-  }
+  double lon;
+  double lat;
 
-  getWeather(String latitude, String longitude) async {
-    setState(() {
-      _loading = true;
-    });
-    var response = await http.get(Uri.parse(
-        "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=b30de56fcbd933743d24fc9004670526&units=metric"));
-    if (response.statusCode == 200) {
-      setState(() {
-        weather = Weather.fromJson(json.decode(response.body));
-      });
-    }
-    setState(() {
-      _loading = false;
-    });
-  }
+  factory Coord.fromJson(Map<String, dynamic> json) => Coord(
+        lon: json["lon"].toDouble(),
+        lat: json["lat"].toDouble(),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "lon": lon,
+        "lat": lat,
+      };
+}
+
+class Main {
+  Main({
+    required this.temp,
+    required this.feelsLike,
+    required this.tempMin,
+    required this.tempMax,
+    required this.pressure,
+    required this.humidity,
+  });
+
+  double temp;
+  double feelsLike;
+  double tempMin;
+  double tempMax;
+  int pressure;
+  int humidity;
+
+  factory Main.fromJson(Map<String, dynamic> json) => Main(
+        temp: json["temp"].toDouble(),
+        feelsLike: json["feels_like"].toDouble(),
+        tempMin: json["temp_min"].toDouble(),
+        tempMax: json["temp_max"].toDouble(),
+        pressure: json["pressure"],
+        humidity: json["humidity"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "temp": temp,
+        "feels_like": feelsLike,
+        "temp_min": tempMin,
+        "temp_max": tempMax,
+        "pressure": pressure,
+        "humidity": humidity,
+      };
+}
+
+class Sys {
+  Sys({
+    required this.country,
+    required this.sunrise,
+    required this.sunset,
+  });
+
+  String country;
+  int sunrise;
+  int sunset;
+
+  factory Sys.fromJson(Map<String, dynamic> json) => Sys(
+        country: json["country"],
+        sunrise: json["sunrise"],
+        sunset: json["sunset"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "country": country,
+        "sunrise": sunrise,
+        "sunset": sunset,
+      };
+}
+
+class WeatherElement {
+  WeatherElement({
+    required this.id,
+    required this.main,
+    required this.description,
+    required this.icon,
+  });
+
+  int id;
+  String main;
+  String description;
+  String icon;
+
+  factory WeatherElement.fromJson(Map<String, dynamic> json) => WeatherElement(
+        id: json["id"],
+        main: json["main"],
+        description: json["description"],
+        icon: json["icon"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "main": main,
+        "description": description,
+        "icon": icon,
+      };
+}
+
+class Wind {
+  Wind({
+    required this.speed,
+    required this.deg,
+  });
+
+  double speed;
+  int deg;
+
+  factory Wind.fromJson(Map<String, dynamic> json) => Wind(
+        speed: json["speed"].toDouble(),
+        deg: json["deg"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "speed": speed,
+        "deg": deg,
+      };
 }
